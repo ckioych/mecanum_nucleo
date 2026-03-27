a95d89e20316000bec14cc87d2242b5d9c690176
ckioych <venyamuradymov@gmail.com>
2026-03-27 11:58:58 +0300

    update

--- mecanum/src/zephyr_encoder.h
+++ mecanum/src/zephyr_encoder.h
@@ -3,23 +3,41 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+// структура энкодера
 typedef struct {
-    const struct device *dev;
-    int32_t last_count;
+    const struct device *gpio_dev;
+    gpio_pin_t pin;
+    struct gpio_callback callback;
+    
+    volatile int32_t counter;
+    volatile int32_t last_count;
     int64_t last_time;
-    int32_t offset;
+    
+    int8_t direction;
 } zephyr_encoder_t;
 
-bool zephyr_encoder_init(zephyr_encoder_t *enc, const struct device *qdec_dev);
+// Инициализация энкодера
+bool zephyr_encoder_init(zephyr_encoder_t *enc, 
+                         const struct device *gpio_dev, 
+                         gpio_pin_t pin);
+
+// получение текущего значение счетчика
 int32_t zephyr_encoder_get_count(zephyr_encoder_t *enc);
+
+// получение скорости
 int32_t zephyr_encoder_get_speed(zephyr_encoder_t *enc);
+
+// сброс счетчика
 void zephyr_encoder_reset(zephyr_encoder_t *enc);
+
+// обновление направления
+void zephyr_encoder_set_direction(zephyr_encoder_t *enc, int8_t dir);
 
 #ifdef __cplusplus
 }
